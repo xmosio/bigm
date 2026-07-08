896ba7a4feb0f3e8caa8219bb930b3b7b9a5ceca
xmosio <xmosio.com@gmail.com>
2026-07-08 05:59:06 +0330

    Update check.py

--- check.py
+++ check.py
@@ -16,8 +16,16 @@ users = {
     "Sahar": "https://bigo.tv/user/437072862",
     "Farimah juju": "https://bigo.tv/user/1079875463",
     "Parisa": "https://bigo.tv/user/691136946",
+    "Blueberry": "https://bigo.tv/user/1042070957",
+    "Hooriya": "https://bigo.tv/user/Hori.91",
+    "Naz ahwaz": "https://bigo.tv/user/731627313",
+    "Sahel": "https://bigo.tv/user/katrina00527",
+    "Mira": "https://bigo.tv/user/857791870l",
+    "Maral": "https://bigo.tv/user/1038185688",
+    "Roya": "https://bigo.tv/user/roya72",
     "Nozhan": "https://bigo.tv/user/nozhanes",
     "Sernay": "https://bigo.tv/user/968395214",
+    "Elnaz": "https://bigo.tv/user/bahaneh",
     "Sadaf": "https://bigo.tv/user/992242250",
     "oos Shaboon": "https://bigo.tv/user/717280198"
 }
@@ -40,7 +48,7 @@ status = {
 }
 
 for name, url in users.items():
-    status["users"][name] = "online" if is_live(url) else "offline"
+    status["users"][name] = "online" if is_live(url) else "__"
 
 with open("status.json", "w") as f:
     json.dump(status, f, indent=2)
