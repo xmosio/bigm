595beffed4647ee5c8ca5e3e9911d543d621bd97
xmosio <xmosio.com@gmail.com>
2026-07-08 06:18:07 +0330

    Update check.py

--- check.py
+++ check.py
@@ -3,30 +3,30 @@ import subprocess
 from datetime import datetime
  
 users = {
-    "❤️ 😋 Sofi": "https://bigo.tv/user/798484012",
-    "🍆 Shabnam": "https://bigo.tv/user/boqzepiano", 
-    "🍆 prMarian": "https://bigo.tv/user/314614931",
+    "Sofi": "https://bigo.tv/user/798484012",
+    "Shabnam": "https://bigo.tv/user/boqzepiano", 
+    "prMarian": "https://bigo.tv/user/314614931",
     "Katrina": "https://bigo.tv/user/imkatrina",
-    "🍆 Taniya": "https://bigo.tv/user/tanitantan",
+    "Taniya": "https://bigo.tv/user/tanitantan",
     "Farimah": "https://bigo.tv/user/1042333738",
     "Zespina": "https://bigo.tv/user/695331268",
     "Elsa": "https://bigo.tv/user/elsa2222",
     "Suzan": "https://bigo.tv/user/suzan11",
     "Lara": "https://bigo.tv/user/onlylara",
     "Sahar": "https://bigo.tv/user/437072862",
-    "😋 Farimah juju": "https://bigo.tv/user/1079875463",
+    "Farimah juju": "https://bigo.tv/user/1079875463",
     "Parisa": "https://bigo.tv/user/691136946",
     "Blueberry": "https://bigo.tv/user/1042070957",
-    "🍆 Hooriya": "https://bigo.tv/user/Hori.91",
-    "🍆 Naz ahwaz": "https://bigo.tv/user/731627313",
+    "Hooriya": "https://bigo.tv/user/Hori.91",
+    "Naz ahwaz": "https://bigo.tv/user/731627313",
     "Sahel": "https://bigo.tv/user/katrina00527",
     "Mira": "https://bigo.tv/user/857791870l",
     "Maral": "https://bigo.tv/user/1038185688",
-    "😋 Roya": "https://bigo.tv/user/roya72",
+    "Roya": "https://bigo.tv/user/roya72",
     "Nozhan": "https://bigo.tv/user/nozhanes",
     "Sernay": "https://bigo.tv/user/968395214",
     "Elnaz": "https://bigo.tv/user/bahaneh",
-    "❤️ 😋 Sadaf": "https://bigo.tv/user/992242250",
+    "Sadaf": "https://bigo.tv/user/992242250",
     "oos Shaboon": "https://bigo.tv/user/717280198"
 }
 
@@ -48,7 +48,7 @@ status = {
 }
 
 for name, url in users.items():
-    status["users"][name] = "online" if is_live(url) else "__"
+    status["users"][name] = "online" if is_live(url) else "."
 
 with open("status.json", "w") as f:
     json.dump(status, f, indent=2)
