94fb41f5ef5c04710ccc1fdb641d3c9a495caf76
xmosio <xmosio.com@gmail.com>
2026-07-26 15:01:53 +0330

    Update check.py

--- check.py
+++ check.py
@@ -54,7 +54,7 @@ status = {
 for name, url in users.items():
     status["users"][name] = "online" if is_live(url) else "offline"
 for name in users.items():
-    if status[users.items("name")] == "offline" users.items("name").remove()
+   if status[users.items(name)] == "offline" users.items(name).remove()
 
 with open("status.json", "w") as f:
     json.dump(status, f, indent=2)
